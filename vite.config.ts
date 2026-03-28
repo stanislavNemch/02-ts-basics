b4273b97444111ee143bb78a36e8ccbb4bcabfc1
Stanislav Nemchenko <stas.buka@gmail.com>
2026-03-29 01:19:22 +0200

    fix: remove base path for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/02-ts-basics/",
   plugins: [react()],
   build: {
     sourcemap: false,
