874113b99cb9da16242bea3fe0e8fea23149df7c
Stanislav Nemchenko <stas.buka@gmail.com>
2026-03-29 01:15:44 +0200

    chore: update package.json and vite.config.ts for improved dependencies and build settings

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        sourcemap: true,
-    },
+  base: "/02-ts-basics/",
+  plugins: [react()],
+  build: {
+    sourcemap: false,
+  },
 });
-
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
